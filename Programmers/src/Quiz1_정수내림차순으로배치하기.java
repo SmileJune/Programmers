913c450c798f3021f72502fb76375555a964f1ef
smilejune <godhkekf24@inha.edu>
2021-11-09 21:36:07 +0900

    Quiz1정수내림차순으로배치하

--- Programmers/src/Quiz1_정수내림차순으로배치하기.java
+++ Programmers/src/Quiz1_정수내림차순으로배치하기.java
@@ -1,10 +1,39 @@
+import java.util.Arrays;
 
 public class Quiz1_정수내림차순으로배치하기 {
 
-	public static void main(String[] args) {
-		long n = 12345678;
-		String n_string = Long.toString(n);
+	public long solution(long n) {
 		
+		
+		String str = Long.toString(n);
+		
+		if(str.length()==1) {
+			return n;
+		}
+		
+		int[] intArray = strTointArray_sorted(str);
+		StringBuffer sb = toStringBuffer_reversed(intArray);
+		String ans_str =  sb.toString();
+		return Long.parseLong(ans_str);
+		
+	}
+
+	private int[] strTointArray_sorted(String str) {
+		int[] intArray = new int[str.length()];
+		for(int i=0; i<str.length(); i++) {
+			intArray[i] = Integer.parseInt(str.substring(i, i+1));
+		}
+		Arrays.sort(intArray);
+		return intArray;
+	}
+
+	private StringBuffer toStringBuffer_reversed(int[] intArray) {
+		StringBuffer sb = new StringBuffer();
+		for(int i=0; i<intArray.length; i++) {
+			sb.append(intArray[i]);
+		}
+		sb.reverse();
+		return sb;
 	}
 
 }
---
+++ Programmers/src/Quiz1_정수내림차순으로배치하기Test.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Quiz1_정수내림차순으로배치하기Test {
+	
+	Quiz1_정수내림차순으로배치하기 q;
+	
+	@BeforeEach
+	public void setup() {
+		q = new Quiz1_정수내림차순으로배치하기();
+	}
+
+	@Test
+	public void 한자리수 () {
+		assertEquals(4,q.solution(4));
+	}
+
+	@Test
+	public void 여러자리수() {
+		assertEquals(843221,q.solution(128234));
+	}
+	
+}
