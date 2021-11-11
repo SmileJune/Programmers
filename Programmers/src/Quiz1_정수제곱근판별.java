27ee9e30eb26bf6a63bcf933c53ba938024ed589
smilejune <godhkekf24@inha.edu>
2021-11-11 23:56:58 +0900

    Quiz1_정수제곱근판별

---
+++ Programmers/src/Quiz1_정수제곱근판별.java
@@ -0,0 +1,21 @@
+
+ public class Quiz1_정수제곱근판별 {
+	
+	 public long solution(long n) {
+		 
+		 long x=-1;
+		 for(int i=1; i<=n/2; i++) {
+			 if(Math.pow(i, 2)==n) {
+				 x=i;
+			 }
+		 }
+		 
+		 if(x==-1) {
+			 return -1;
+		 }
+		 
+		 
+		 return (long) Math.pow(x+1, 2);
+		 
+	    }
+	}
---
+++ Programmers/src/Quiz1_정수제곱근판별Test.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Quiz1_정수제곱근판별Test {
+
+	Quiz1_정수제곱근판별 q;
+	
+	@BeforeEach
+	public void setup() {
+		q = new Quiz1_정수제곱근판별();
+	}
+	
+	@Test
+	public void whatIsX() {
+		assertEquals(36,q.solution(25));
+	}
+	
+	@Test
+	public void noX() {
+		assertEquals(-1,q.solution(26));
+	}
+
+}
