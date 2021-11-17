7ee116e8e0a274295dac3cd4d88a87eec5145f92
smilejune <godhkekf24@inha.edu>
2021-11-17 22:52:41 +0900

    Quiz1_평균구하기_"작지만 메소드 이름을 잘 지어보려 노력했다."

---
+++ Programmers/src/Quiz1_평균구하기.java
@@ -0,0 +1,14 @@
+
+public class Quiz1_평균구하기 {
+	 public double solution(int[] arr) {
+		 return sumOfArrayElements(arr)/arr.length;
+	 }
+	 
+	 private double sumOfArrayElements(int[] arr) {
+		 double sum=0;
+		 for(int element : arr) {
+			 sum+=element;
+		 }
+		 return sum;
+	}	
+}
---
+++ Programmers/src/Quiz1_평균구하기Test.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Quiz1_평균구하기Test {
+	
+	Quiz1_평균구하기 q;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		q=new Quiz1_평균구하기();
+	}
+
+	@Test
+	void testOneElement() {
+		assertEquals(3,q.solution(new int[] {3}));
+	}
+	@Test
+	void test1234() {
+		assertEquals(2.5,q.solution(new int[] {1,2,3,4}));
+	}
+	@Test
+	void test55() {
+		assertEquals(5,q.solution(new int[] {5,5}));
+	}
+
+}
