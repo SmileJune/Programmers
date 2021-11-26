585dfbfd468df53f2691450b599268b11ca2b4d9
smilejun <godhkekf24@inha.edu>
2021-11-26 22:59:52 +0900

    Quiz1_K번째수

---
+++ Programmers/test/Quiz1_KthNumberTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Quiz1_KthNumberTest {
+    Quiz1_KthNumber q;
+
+    @BeforeEach
+    void setUp() {
+        q = new Quiz1_KthNumber();
+    }
+
+    @Test
+    void test1() {
+        assertArrayEquals(new int[]{5, 6, 3}, q.solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}));
+    }
+}
