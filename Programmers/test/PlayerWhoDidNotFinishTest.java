a35f8d90801a7b64bf7ccbcb10bbee277522fb93
smilejun <godhkekf24@inha.edu>
2021-11-27 22:27:02 +0900

    Quiz1_완주하지 못한 선수

---
+++ Programmers/test/PlayerWhoDidNotFinishTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerWhoDidNotFinishTest {
+
+    PlayerWhoDidNotFinish q;
+
+    @BeforeEach
+    void setUp() {
+        q = new PlayerWhoDidNotFinish();
+    }
+
+    @Test
+    void test() {
+        assertEquals(new String("leo"), q.solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}));
+    }
+}
