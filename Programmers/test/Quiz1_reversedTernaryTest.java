6de19d9e65ae348e3ae3e6e69cb3df0f3dc6af6a
smilejun <godhkekf24@inha.edu>
2021-12-01 00:08:22 +0900

    Quiz1_3진법 뒤집기

---
+++ Programmers/test/Quiz1_reversedTernaryTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Quiz1_reversedTernaryTest {
+
+    Quiz1_reversedTernary q;
+
+    @BeforeEach
+    void setUp() {
+        q = new Quiz1_reversedTernary();
+    }
+
+    @Test
+    void test1() {
+        assertEquals(7, q.solution(45));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(229, q.solution(125));
+    }
+
+}
