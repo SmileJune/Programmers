e17ca0c668f749c5723a5e9c6f4677c840c59616
smilejun <godhkekf24@inha.edu>
2021-12-08 14:16:23 +0900

    Quiz_1 시저 암호

---
+++ Programmers/test/Quiz1_CaesarCipherTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Quiz1_CaesarCipherTest {
+
+    Quiz1_CaesarCipher q;
+
+    @BeforeEach
+    void setUp() {
+        q = new Quiz1_CaesarCipher();
+    }
+
+    @Test
+    void test1() {
+        assertEquals(new String("BC"), q.solution(new String("AB"), 1));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(new String("a"), q.solution(new String("z"), 1));
+    }
+
+    @Test
+    void test3() {
+        assertEquals(new String("e F d"), q.solution(new String("a B z"), 4));
+    }
+}
---
+++ Programmers/test/Quiz1_CaesarCipher_2Test.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Quiz1_CaesarCipher_2Test {
+
+    Quiz1_CaesarCipher q;
+
+    @BeforeEach
+    void setUp() {
+        q = new Quiz1_CaesarCipher();
+    }
+
+    @Test
+    void test1() {
+        assertEquals(new String("BC"), q.solution(new String("AB"), 1));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(new String("a"), q.solution(new String("z"), 1));
+    }
+
+    @Test
+    void test3() {
+        assertEquals(new String("e F d"), q.solution(new String("a B z"), 4));
+    }
+}
