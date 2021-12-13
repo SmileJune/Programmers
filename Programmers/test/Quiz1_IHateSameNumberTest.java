fec3999147a888b0688de985531432495959496d
smilejun <godhkekf24@inha.edu>
2021-12-13 21:14:14 +0900

    Quiz1_같은 숫자는 싫어

---
+++ Programmers/test/Quiz1_IHateSameNumberTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Quiz1_IHateSameNumberTest {
+
+    Quiz1_IHateSameNumber q;
+
+    @BeforeEach
+    void setUp() {
+        q = new Quiz1_IHateSameNumber();
+    }
+
+    @Test
+    void 한개() {
+        assertArrayEquals(new int[]{5}, q.solution(new int[]{5}));
+    }
+
+    @Test
+    void test1() {
+        assertArrayEquals(new int[]{1, 3, 0, 1}, q.solution(new int[]{1, 1, 3, 3, 0, 1, 1}));
+    }
+
+    @Test
+    void test2() {
+        assertArrayEquals(new int[]{4, 3}, q.solution(new int[]{4, 4, 4, 3, 3}));
+    }
+}
