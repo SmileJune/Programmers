523f295a4faeeeb5023d719286ff92cd6dbcaf91
smilejun <godhkekf24@inha.edu>
2021-12-16 00:31:43 +0900

    Quiz1_문자열 내 마음대로 정렬하기

---
+++ Programmers/test/Quiz1_SortingStringsTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Quiz1_SortingStringsTest {
+
+    Quiz1_SortingStrings q;
+
+    @BeforeEach
+    void setUp() {
+        q = new Quiz1_SortingStrings();
+    }
+
+    @Test
+    void test1() {
+        assertArrayEquals(new String[]{"car", "bed", "sun"}, q.solution(new String[]{"sun", "bed", "car"}, 1));
+    }
+
+    @Test
+    void test2() {
+        assertArrayEquals(new String[]{"abcd", "abce", "cdx"}, q.solution(new String[]{"abce", "abcd", "cdx"}, 2));
+    }
+
+}
